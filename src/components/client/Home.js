4cc8586f46631f61b21073265716684c4d6b7a7c
Casprovy <casprovy.tgs@gmail.com>
2019-01-29 19:15:53 +0100

    b4 fetching mock data

--- src/components/client/Home.js
+++ src/components/client/Home.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios';
 import Layout from './Layout';
 import CategoryContainer from './containers/category';
 import ProductContainer from './containers/product';
+import { apiConstants } from '../../constants/api.constants';
 
 
-const Home = () => {
+export default class Home extends Component {
+
+componentDidMount () {
+    axios.get(apiConstants.PRODUCTS_URL)
+    .then(json => console.log(json))
+}
+
+    render() {
     return (
         <Layout>
             <CategoryContainer />
@@ -12,7 +21,7 @@ const Home = () => {
             <ProductContainer />
         </Layout>
 
-    );
-};
+        );
+        };
+    }
 
-export default Home;
--- src/constants/api.constants.js
+++ src/constants/api.constants.js
@@ -1,4 +1,4 @@
 export const apiConstants = {
   CATEGORIES_URL: '.......',
-  PRODUCTS_URL: '.....'
+  PRODUCTS_URL: 'https://private-6342ba-luca26.apiary-mock.com/products'
 }
