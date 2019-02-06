ddfde07419bd528b761d37b7648faa7a458ab839
Zain <chelnov63@gmail.com>
2019-02-06 18:50:39 +0100

    changes

--- src/components/client/orders/index.js
+++ src/components/client/orders/index.js
@@ -25,13 +25,8 @@ class Orders extends Component {
 
         return (
             <Layout>
-                <div className="theForm">
-                    <fieldset>
-                        <legend><span className="number"></span>Previous Orders</legend>
-                    </fieldset>
-                    {this.state.orders.map(order => (<Order {...order} key={order.order_num} />))}
-                    {!this.state.orders.length && "You have no previous orders at present"}
-                </div>
+                {this.state.orders.map(order => (<Order {...order} key={order.order_num} />))}
+                {!this.state.orders.length && "You have no previous orders at present"}
             </Layout>
         );
     }
