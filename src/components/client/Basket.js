d5584a1b3329806f0f0bde7e6e93d7698d8f940f
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-02-07 15:55:58 +0100

    Edit the `Basket.js` and `prod-basket` styling.

--- src/components/client/Basket.js
+++ src/components/client/Basket.js
@@ -21,7 +21,10 @@ class Basket extends Component {
       return <div className="basketcnt"><i class="material-icons grey-text large">shopping_basket</i></div>
     } else {
       return this.props.basket.map((item, index) => {
-        return <li className="collection-item avatar" key={index}><ProdBasket {...item} /></li>
+        return (
+          <div className="" key={index}>
+            <ProdBasket {...item} />
+          </div>)
       })
     }
   }
@@ -46,32 +49,31 @@ class Basket extends Component {
         <div className="container">
           <div className="row">
 
-            <div className="col s12">
+            <div className="col s12 basket-column">
 
               <div className="col s12 m8 l6">
 
-                <ul className="collection">
+                <React.Fragment className="collection">
                   {this.showBasket()}
-                </ul>
+                </React.Fragment>
 
               </div>
-
               <div className="card col s12 m8 l6" id="basket">
                 <span className="card-title">Your basket</span>
 
                 <div className="card-content">
                   <div className="basket-summary">
                     <div>
-                      <p className="black-text">Items (3): </p>
+                      <p className="black-text">Items: </p>
                       <p className="black-text">Shipping & handling: </p>
                       <p className="black-text">Total before tax: </p>
                       <p className="black-text">Tax to be collected: </p>
                     </div>
                     <div id="summary-amounts">
-                      <p className="black-text">$37.00</p>
-                      <p className="black-text" id="shipping-handling">$ 45.00</p>
-                      <p className="black-text">$ 112.0</p>
-                      <p className="black-text">$ 4.00</p>
+                      <p className="black-text">€ {this.totalPrice()}</p>
+                      <p className="black-text" id="shipping-handling">€ 0.00</p>
+                      <p className="black-text">€ 0.00</p>
+                      <p className="black-text">€ {this.totalPrice()}</p>
                     </div>
                   </div>
 
@@ -80,11 +82,11 @@ class Basket extends Component {
                       <h5 className="basket-total">Order total:</h5>
                     </div>
                     <div>
-                      <h5 className="basket-total">{this.totalPrice()}</h5>
+                      <h5 className="basket-total">{this.totalPrice()} €</h5>
                     </div>
                   </div>
 
-                  <Link to={`/checkout/${this.totalPrice()}`} className={`waves-effect waves-light btn ${this.checkoutZero()}`} id="checkout-btn"><i className="material-icons left"></i>Proceed to Checkout</Link>
+                  <Link to={`/checkout/${this.totalPrice()}`} className={`waves-effect waves-light btn ${this.checkoutZero()} amber`} id="checkout-btn"><i className="material-icons left"></i>Proceed to Checkout</Link>
                   {/* <Link to="/" className="waves-effect waves-light btn blue lighten-2"><i className="material-icons left"></i>Cancel</Link> */}
                 </div>
               </div>
@@ -109,4 +111,3 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Basket)
-
