415740990f66d8fd0357d8219d77e313afd998b5
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-02-07 12:22:46 +0100

    Edit the cs constants file

--- src/components/client/Layout.js
+++ src/components/client/Layout.js
@@ -209,9 +209,9 @@ class Layout extends Component {
           </div>
           <div class="social">
             <a href="#" class="support">Contact Us</a>
-            <a href="#" class="face">f</a>
-            <a href="#" class="tweet">t</a>
-            <a href="#" class="linked">in</a>
+            <a href="#" className="face">f</a>
+            <a href="#" className="tweet">t</a>
+            <a href="#" className="linked">in</a>
           </div>
         </footer>
 
